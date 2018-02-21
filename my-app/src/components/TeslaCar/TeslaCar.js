fa6a172f7f28ca29de359a490b787385ea5849af
oliwiah <oliwia.hajszprych@gmail.com>
2018-02-21 01:19:07 +0100

    animated wheels and buttons

--- my-app/src/components/TeslaCar/TeslaCar.js
+++ my-app/src/components/TeslaCar/TeslaCar.js
@@ -4,12 +4,13 @@ import './TeslaCar.css';
 
 const TeslaCar = (props) => (
     <div className="tesla-car tesla-car-animation">
-        <div className="tesla-wheels">
+        <div className={`tesla-wheels tesla-wheel--animation--${props.wheelsize}`}>
             <div className={`tesla-wheel tesla-wheel--front tesla-wheel--${props.wheelsize}`}></div>
             <div className={`tesla-wheel tesla-wheel--rear tesla-wheel--${props.wheelsize}`}></div>
         </div>
     </div>
 );
+
 TeslaCar.propTypes = {
     wheelsize: PropTypes.number
 }
--- my-app/src/components/TeslaCounter/TeslaCounter.js
+++ my-app/src/components/TeslaCounter/TeslaCounter.js
@@ -2,31 +2,69 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './TeslaCounter.css';
 
-const TeslaCounter = (props) => (
-    <div className="tesla-counter">
-        <p className="tesla-counter__title">{ props.initValues.title }</p>
-        <div className="tesla-counter__container cf">
-            <div className="tesla-counter__item">
-                <p className="tesla-counter__number">
-                    { props.currentValue }
-                    <span>{ props.initValues.unit }</span>
-                </p>
-                <div className="tesla-counter__controls">
-                    <button
-                        onClick={(e) => props.increment(e, props.initValues.title)}
-                        disabled={props.currentValue >= props.initValues.max}
-                    >
-                    </button>
-                    <button
-                        onClick={(e) => props.decrement(e, props.initValues.title)}
-                        disabled={props.currentValue <= props.initValues.min}
-                    >
-                    </button>
+class TeslaCounter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            animationEffect: false,
+            direction: ''
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const { currentValue, initValues } = nextProps;
+        if (this.props.currentValue !== nextProps.currentValue) {
+            const direction = this.props.currentValue < nextProps.currentValue ? 'increase' : 'decrese';
+            this.setState({
+                animationEffect: true,
+                direction
+            });
+
+            setTimeout(
+                () => {
+                    this.setState({
+                        animationEffect: false
+                    })
+                }, 200
+            );
+            return;
+        }
+    }
+
+    render() {
+        const { initValues, currentValue, increment, decrement } = this.props;
+        const { animationEffect, direction } = this.state;
+        const animationClass = animationEffect
+            ? (direction==='increase'? 'speed-up-wheel-animation' : 'speed-down-wheel-animation')
+            : '';
+
+        return (
+            <div className="tesla-counter">
+                <p className="tesla-counter__title">{initValues.title}</p>
+                <div className="tesla-counter__container cf">
+                    <div className="tesla-counter__item">
+                        <div className={`${animationClass}`}>
+                            <p className="tesla-counter__number">
+                                {currentValue}
+                                <span>{initValues.unit}</span>
+                            </p>
+                        </div>
+                        <div className="tesla-counter__controls">
+                            <button
+                                onClick={(e) => increment(e, initValues.title)}
+                                disabled={currentValue >= initValues.max}>
+                            </button>
+                            <button
+                                onClick={(e) => decrement(e, initValues.title)}
+                                disabled={currentValue <= initValues.min}>
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-);
+        )
+    }
+};
 
 TeslaCounter.propTypes = {
     currentValue: PropTypes.number,
@@ -35,31 +73,71 @@ TeslaCounter.propTypes = {
     initValues: PropTypes.object
 }
 
-const TeslaCounter2 = (props) => (
-    <div className="tesla-counter">
-        <p className="tesla-counter__title" id="hidden">{ props.initValues.title }</p>
-        <div className="tesla-counter__container cf">
-            <div className="tesla-counter__item">
-                <p className="tesla-counter__number">
-                    { props.currentValue }
-                    <span id="km"> km/h </span>
-                </p>
-                <div className="tesla-counter__controls">
-                    <button
-                        onClick={(e) => props.increment(e, props.initValues.title)}
-                        disabled={props.currentValue >= 113}
-                    >
-                    </button>
-                    <button
-                        onClick={(e) => props.decrement(e, props.initValues.title)}
-                        disabled={props.currentValue <= 72}
-                    >
-                    </button>
+class TeslaCounter2 extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            animationEffect: false,
+            direction: ''
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const { currentValue, initValues } = nextProps;
+        if (this.props.currentValue !== nextProps.currentValue) {
+            const direction = this.props.currentValue < nextProps.currentValue ? 'increase' : 'decrese';
+            this.setState({
+                animationEffect: true,
+                direction
+            });
+
+            setTimeout(
+                () => {
+                    this.setState({
+                        animationEffect: false
+                    })
+                }, 200
+            );
+            return;
+        }
+    }
+
+    render() {
+        const { initValues, currentValue, increment, decrement } = this.props;
+        const { animationEffect, direction } = this.state;
+        const animationClass = animationEffect
+            ? (direction==='increase'? 'speed-up-wheel-animation' : 'speed-down-wheel-animation')
+            : '';
+
+        return (
+            <div className="tesla-counter">
+                <p className="tesla-counter__title" id="hidden">{initValues.title}</p>
+                <div className="tesla-counter__container cf">
+                    <div className="tesla-counter__item">
+                        <div className={`${animationClass}`}>
+                            <p className="tesla-counter__number">
+                                {currentValue}
+                                <span id="km"> km/h </span>
+                            </p>
+                        </div>
+                        <div className="tesla-counter__controls">
+                            <button
+                                onClick={(e) => increment(e, initValues.title)}
+                                disabled={currentValue >= 113}
+                            >
+                            </button>
+                            <button
+                                onClick={(e) => decrement(e, initValues.title)}
+                                disabled={currentValue <= 72}
+                            >
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-);
+        )
+    }
+};
 
 TeslaCounter2.propTypes = {
     currentValue: PropTypes.number,
@@ -68,31 +146,71 @@ TeslaCounter2.propTypes = {
     initValues: PropTypes.object
 }
 
-const TeslaCounter3 = (props) => (
-    <div className="tesla-counter">
-        <p className="tesla-counter__title">{ props.initValues.title }</p>
-        <div className="tesla-counter__container cf">
-            <div className="tesla-counter__item">
-                <p className="tesla-counter__number">
-                    { props.currentValue }
-                    <span id={"km"}>{ props.initValues.unit } c</span>
-                </p>
-                <div className="tesla-counter__controls">
-                    <button
-                        onClick={(e) => props.increment(e, props.initValues.title)}
-                        disabled={props.currentValue >= props.initValues.max}
-                    >
-                    </button>
-                    <button
-                        onClick={(e) => props.decrement(e, props.initValues.title)}
-                        disabled={props.currentValue <= props.initValues.min}
-                    >
-                    </button>
+class TeslaCounter3 extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            animationEffect: false,
+            direction: ''
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const {currentValue, initValues} = nextProps;
+        if (this.props.currentValue !== nextProps.currentValue) {
+            const direction = this.props.currentValue < nextProps.currentValue ? 'increase' : 'decrese';
+            this.setState({
+                animationEffect: true,
+                direction
+            });
+
+            setTimeout(
+                () => {
+                    this.setState({
+                        animationEffect: false
+                    })
+                }, 200
+            );
+            return;
+        }
+    }
+
+    render() {
+        const {initValues, currentValue, increment, decrement} = this.props;
+        const {animationEffect, direction} = this.state;
+        const animationClass = animationEffect
+            ? (direction === 'increase' ? 'speed-up-wheel-animation' : 'speed-down-wheel-animation')
+            : '';
+
+        return (
+            <div className="tesla-counter">
+                <p className="tesla-counter__title">{initValues.title}</p>
+                <div className="tesla-counter__container cf">
+                    <div className="tesla-counter__item">
+                        <div className={`${animationClass}`}>
+                            <p className="tesla-counter__number">
+                                {currentValue}
+                                <span id={"km"}>{initValues.unit} c</span>
+                            </p>
+                        </div>
+                        <div className="tesla-counter__controls">
+                            <button
+                                onClick={(e) => increment(e, initValues.title)}
+                                disabled={currentValue >= initValues.max}
+                            >
+                            </button>
+                            <button
+                                onClick={(e) => decrement(e, initValues.title)}
+                                disabled={currentValue <= initValues.min}
+                            >
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-);
+        )
+    }
+};
 
 TeslaCounter3.propTypes = {
     currentValue: PropTypes.number,
@@ -101,31 +219,71 @@ TeslaCounter3.propTypes = {
     initValues: PropTypes.object
 }
 
-const TeslaCounter4 = (props) => (
-    <div className="tesla-counter">
-        <p className="tesla-counter__title" id="hidden">{ props.initValues.title }</p>
-        <div className="tesla-counter__container cf">
-            <div className="tesla-counter__item">
-                <p className="tesla-counter__number">
-                    { props.currentValue }
-                    <span id="km">{ props.initValues.unit } f </span>
-                </p>
-                <div className="tesla-counter__controls">
-                    <button
-                        onClick={(e) => props.increment(e, props.initValues.title)}
-                        disabled={props.currentValue >= 104}
-                    >
-                    </button>
-                    <button
-                        onClick={(e) => props.decrement(e, props.initValues.title)}
-                        disabled={props.currentValue <= 14}
-                    >
-                    </button>
+class TeslaCounter4 extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            animationEffect: false,
+            direction: ''
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const {currentValue, initValues} = nextProps;
+        if (this.props.currentValue !== nextProps.currentValue) {
+            const direction = this.props.currentValue < nextProps.currentValue ? 'increase' : 'decrese';
+            this.setState({
+                animationEffect: true,
+                direction
+            });
+
+            setTimeout(
+                () => {
+                    this.setState({
+                        animationEffect: false
+                    })
+                }, 200
+            );
+            return;
+        }
+    }
+
+    render() {
+        const {initValues, currentValue, increment, decrement} = this.props;
+        const {animationEffect, direction} = this.state;
+        const animationClass = animationEffect
+            ? (direction === 'increase' ? 'speed-up-wheel-animation' : 'speed-down-wheel-animation')
+            : '';
+
+        return (
+            <div className="tesla-counter">
+                <p className="tesla-counter__title" id="hidden">{initValues.title}</p>
+                <div className="tesla-counter__container cf">
+                    <div className="tesla-counter__item">
+                        <div className={`${animationClass}`}>
+                            <p className="tesla-counter__number">
+                                {currentValue}
+                                <span id="km">{initValues.unit} f </span>
+                            </p>
+                        </div>
+                        <div className="tesla-counter__controls">
+                            <button
+                                onClick={(e) => increment(e, initValues.title)}
+                                disabled={currentValue >= 104}
+                            >
+                            </button>
+                            <button
+                                onClick={(e) => decrement(e, initValues.title)}
+                                disabled={currentValue <= 14}
+                            >
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-);
+        )
+    }
+};
 
 TeslaCounter4.propTypes = {
     currentValue: PropTypes.number,
